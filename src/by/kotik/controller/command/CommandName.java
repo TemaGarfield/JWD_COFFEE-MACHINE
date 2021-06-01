9716eb0ec72776d03d5ae46d51c75b34c7ae2502
TemaGarfield <49874332+TemaGarfield@users.noreply.github.com>
2021-06-01 11:05:48 +0300

    Delete src/by/kotik directory

--- src/by/kotik/controller/command/CommandName.java
+++
@@ -1,5 +0,0 @@
-package by.kotik.controller.command;
-
-public enum CommandName {
-    LOGINATION, REGISTRATION, GOTOMAINPAGE, SAVENEWUSER, LOGIN
-}
--- src/by/kotik/dao/DBDriverLoadingException.java
+++
@@ -1,7 +0,0 @@
-package by.kotik.dao;
-
-public class DBDriverLoadingException extends RuntimeException{
-    public DBDriverLoadingException(Exception e) {
-        super(e);
-    }
-}
