9dce84108cce20ee923fcc7731810738ddf936b5
TemaGarfield <49874332+TemaGarfield@users.noreply.github.com>
2021-06-09 10:09:45 +0300

    Delete src/main/java/by/kotik/main/java/by/kotik directory

--- src/main/java/by/kotik/main/java/by/kotik/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package by.kotik.service;
-
-import by.kotik.bean.User;
-
-public interface UserService {
-    public User authorization(String login, String password) throws ServiceException;
-    public boolean registration(User user) throws ServiceException;
-
-}
